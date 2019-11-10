914538363c1237278d778f89ba78da45d2bd646a
Sofiya Marchanka <sofiya.belous@gmail.com>
2019-11-10 00:31:25 -0500

    Added server implementation

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
-const express = require('express');
-const path = require('path');
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('./src/comments.json');
+const middlewares = jsonServer.defaults({ static: './dist/comment-app'});
+const port = process.env.PORT || 3000;
 
-const app = express();
+server.use(middlewares);
+server.use(router);
 
-app.use(express.static(__dirname + '/dist/comment-app'));
-
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/comment-app/index.html'));
-});
-
-app.listen(process.env.PORT || 8080);
+server.listen(port);

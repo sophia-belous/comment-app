bffc0830f682ac451679980d40cafd861ebfc174
Sofiya Marchanka <sofiya.belous@gmail.com>
2019-11-10 00:46:41 -0500

    Fixed api request

--- src/app/services/comments.service.ts
+++ src/app/services/comments.service.ts
@@ -6,23 +6,21 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class CommentsService {
-  url = 'http://localhost:3000';
-
   constructor(private http: HttpClient) {}
 
   getAll(): Observable<any> {
-    return this.http.get(`${this.url}/comments`);
+    return this.http.get(`/comments`);
   }
 
   addComment(data) {
-    return this.http.post(`${this.url}/comments`, data);
+    return this.http.post(`/comments`, data);
   }
 
   editComment(data) {
-    return this.http.put(`${this.url}/comments/${data.id}`, data);
+    return this.http.put(`/comments/${data.id}`, data);
   }
 
   deleteComment(id) {
-    return this.http.delete(`${this.url}/comments/${id}`);
+    return this.http.delete(`/comments/${id}`);
   }
 }
